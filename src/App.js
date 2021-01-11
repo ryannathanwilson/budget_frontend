3ad00dc1253af2e4af791de9792bc742d382c9ba
ryannathanwilson <ryan.nathan.wilson@gmail.com>
2021-01-11 09:43:03 -0500

    create boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/global.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="RNW REACT">
+        <h1>
+			RNW REACT TEMPLATE
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+	  		Updated 11 January 2021
         </a>
-      </header>
     </div>
   );
 }
