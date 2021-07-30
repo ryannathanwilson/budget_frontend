a651d46b37a427dc0c09e6f343547faf14898528
ryannathanwilson <ryan.nathan.wilson@gmail.com>
2021-07-30 16:27:07 -0400

    start setting up aesthetics

--- src/components/ViewBalances.js
+++ src/components/ViewBalances.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const ViewBalances = (props) => {
     const [moneyData, setMoneyData] = useState({});
-	const [myInput, setMyInput] = useState("");
+    const [myInput, setMyInput] = useState("");
     const getMoneyData = () => {
         fetch("http://127.0.0.1:8000/api/get_money_data/", {
             // fetch("http://127.0.0.1:8000/api/current_user/", {
@@ -26,18 +26,31 @@ const ViewBalances = (props) => {
     }, []);
     useEffect(() => {
         let example = moneyData;
-        if (typeof(example) === "object" && example !== null) {
-			console.log(example);
+        if (typeof example === "object" && example !== null) {
+            console.log(example);
             if ("Food" in example) {
-				setMyInput(example.Food.thisMonthExpenses);
+                setMyInput(example.Food.thisMonthExpenses);
             }
         }
     }, [moneyData]);
 
     return (
-        <div>
+        <div id="view-balances">
             <h1>Here's the situation</h1>
-            <div>{myInput}</div>
+            {Object.keys(moneyData).map((keyName, i) => (
+                <div key={i}>
+                    <div className="balance-card">
+                        <div className="category">{keyName.toUpperCase()}</div>
+                        <div className="remaining">
+                            ${moneyData[keyName].totalRemainingBudget} left
+                        </div>
+                    </div>
+                    <ul>
+                        <li>Expenses this month: ${moneyData[keyName].thisMonthExpenses}</li>
+                        <li>Budget this month: ${moneyData[keyName].thisMonthBudget}</li>
+                    </ul>
+                </div>
+            ))}
         </div>
     );
 };
